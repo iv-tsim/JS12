7b4dc803d8b8214c45f8ff0410a551ee1a7a61f7
Ivan Tsimbalist <iv-tsim@yandex.ru>
2020-03-30 12:33:54 +0300

    add corrections to 3rd lesson

--- lesson03/script.js
+++ lesson03/script.js
@@ -1,26 +1,16 @@
-let money,
-    income = "Листовки",
-    addExpenses,
-    deposit,
-    mission = 200000,
-    period = 6,
-    budgetDay,
-    missionComplete;
-
-money = prompt("Ваш месячный доход?");
-addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
-deposit = confirm("Есть ли у вас депозит в банке?");
-
-let expenses1 = prompt("Введите обязательную статью расходов");
-let amount1 = prompt("Во сколько это обойдется?");
-let expenses2 = prompt("Введите обязательную статью расходов");
-let amount2 = prompt("Во сколько это обойдется?");
-
-let budgetMonth = +money - +amount1 - +amount2;
-
-missionComplete = Math.ceil(mission / budgetMonth)
-
-budgetDay = Math.floor(budgetMonth / 30);
+const   money = +prompt("Ваш месячный доход?"),
+        income = "Листовки",
+        addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую"),
+        deposit = confirm("Есть ли у вас депозит в банке?"),
+        mission = 200000,
+        period = 6,
+        expenses1 = prompt("Введите обязательную статью расходов"),
+        amount1 = +prompt("Во сколько это обойдется?"),
+        expenses2 = prompt("Введите обязательную статью расходов"),
+        amount2 = +prompt("Во сколько это обойдется?");
+let budgetMonth = money - amount1 - amount2,
+    budgetDay = Math.floor(budgetMonth / 30),
+    missionComplete = Math.ceil(mission / budgetMonth);
 
 console.log(typeof money);
 console.log(typeof income);
@@ -31,8 +21,7 @@ console.log(addExpenses.length);
 console.log("Период равен " + period + " месяцев");
 console.log("Цель заработать " + mission + " рублей/долларов/гривен/юани");
 
-addExpenses = addExpenses.toLowerCase();
-console.log(addExpenses.split(","));
+console.log(addExpenses.toLowerCase().split(","));
 
 console.log("Бюджет на месяц: ", budgetMonth);
 
@@ -43,7 +32,7 @@ if(budgetDay >= 1200) {
     console.log("У вас высокий уровень дохода");
 } else if(budgetDay >= 600) {
     console.log("У вас средний уровень дохода");
-} else if(budgetDay < 600 || budgetDay >= 0 ) {
+} else if(budgetDay < 600 && budgetDay >= 0 ) {
     console.log("К сожалению у вас уровень дохода ниже среднего");
 } else {
     console.log("Что то пошло не так");
