198b0a85fb8aaa49109783896baeaf87b4ff2056
Ivan Tsimbalist <iv-tsim@yandex.ru>
2020-05-02 02:40:19 +0300

    complete lesson23

--- lesson22/js/script.js
+++ lesson22/js/script.js
@@ -209,12 +209,49 @@ window.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
-    const culc = () => {
+    const culc = (price = 100) => {
         const calcItems = document.querySelectorAll("input.calc-item");
         calcItems.forEach((item) => {
             item.addEventListener("input", () => {
                 item.value = item.value.replace(/\D/g, "");
             })
+        });
+        const   calcBlock = document.querySelector(".calc-block"),
+                calcType = document.querySelector(".calc-type"),
+                calcSquare = document.querySelector(".calc-square"),
+                calcDay = document.querySelector(".calc-day"),
+                calcCount = document.querySelector(".calc-count"),
+                totalValue = document.getElementById("total");
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+
+            const   typeValue = calcType.options[calcType.selectedIndex].value,
+                    squareValue = +calcSquare.value;
+            
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+            totalValue.textContent = Math.ceil(total);
+        }
+
+        calcBlock.addEventListener("change", (event) => {
+            const target = event.target;
+            if (target.matches("select") || target.matches("input")) {
+                countSum();
+            }
         });
     }
     countTimer("30 April 2020 6:42:0");
@@ -223,5 +260,5 @@ window.addEventListener("DOMContentLoaded", function() {
     tabs();
     slider();
     photoChange();
-    culc();
+    culc(100);
 });
