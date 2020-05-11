6eb1c4664455c7021dbc585e4662e9b66be7bacf
Ivan Tsimbalist <iv-tsim@yandex.ru>
2020-05-12 01:21:25 +0300

    optimized event listeners for menu again

--- lesson26/js/script.js
+++ lesson26/js/script.js
@@ -35,12 +35,12 @@ window.addEventListener("DOMContentLoaded", function() {
                 handlerMenu = () => menu.classList.toggle("active-menu");
         document.addEventListener("click", (event) => {
             let target = event.target;
-            if (target.parentNode.classList.contains("menu") || target.classList.contains("close-btn") || target.matches("a")) {
+            if (target.parentNode.classList.contains("menu") || target.classList.contains("close-btn") || target.matches("li a")) {
                 handlerMenu();
             } else if (target !== menu && menu.classList.contains("active-menu")) {
                 handlerMenu();
             }
-            if (target.closest("a") && target.closest("a").getAttribute("href") !== "#close") {
+            if (target.closest("a") && target.closest("a").getAttribute("href") !== "#close" && target.closest("a").getAttribute("href") !== "#" && target.closest("a").getAttribute("href") !== "") {
                 event.preventDefault();
                 document.querySelector(target.closest("a").getAttribute("href")).scrollIntoView({block: "start", behavior: "smooth"});
             }
